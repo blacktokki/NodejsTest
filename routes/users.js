781c03bcdc82f38ea82bc007bced1c68694de770
namuwiky <39031723+namuwiky@users.noreply.github.com>
2019-07-25 15:50:05 +0900

    express 연습2-회원가입

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const models = require("../models");
+
+
+router.get('/sign_up', function(req, res, next) {
+  res.render("user/signup");
+});
+
+
+router.post("/sign_up", async function(req,res,next){
+  let body = req.body;
+
+  let inputPassword = body.password;
+  let salt = Math.round((new Date().valueOf() * Math.random())) + "";
+  let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+
+  let result = models.user.create({
+    name: body.userName,
+    email: body.userEmail,
+    password: hashPassword,
+    salt: salt
+  })
+
+  res.redirect("/users/sign_up");
+})
+
+module.exports = router;
